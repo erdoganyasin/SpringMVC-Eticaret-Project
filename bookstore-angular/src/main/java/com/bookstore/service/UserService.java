2d937a1c487ee7258f4085d7bf250414c8ef95ce
Yasin Erdogan <erdogan_yasin@hotmail.com>
2018-05-08 03:26:44 +0300

    ilk gonderme

---
+++ bookstore-angular/src/main/java/com/bookstore/service/UserService.java
@@ -0,0 +1,12 @@
+package com.bookstore.service;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import com.bookstore.domain.User;
+import com.bookstore.domain.security.UserRole;
+
+public interface UserService {
+	User createUser(User user, Set<UserRole> userRoles);
+}
